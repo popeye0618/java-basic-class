e126c65316b3fff3ccd89884c781ffabaa76a9e9
popeye0618 <popeye0618@gmail.com>
2024-12-09 12:26:36 +0900

    day1

---
+++ src/week1/day1/Day1Practice.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Day1Practice {
+    public static void main(String[] args) {
+
+        // 자기소개 (이름, 나이, 성별)
+        System.out.println("저는 25살 남자 홍길동입니다.");
+
+        // 산수 문제와 답 출력
+        System.out.println("3 + 2 = 5");
+        System.out.println("3 - 2 = 1");
+        System.out.println("3 * 2 = 6");
+        System.out.println("6 / 2 = 3");
+        System.out.println("5 % 2 = 1");
+    }
+}
---
+++ src/week1/day1/Day1Print.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Day1Print {
+    public static void main(String[] args) {
+        // 출력 함수의 기본 사용법
+        System.out.println("Hello world!!"); // 줄 바꿈과 함께 출력
+        System.out.print("Hello world!\n"); // 줄 바꿈 없이 출력
+    }
+}
+
