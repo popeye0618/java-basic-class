92d6f80f458de3bb601c813fdc004919c6d4bca6
popeye0618 <popeye0618@gmail.com>
2024-12-15 12:11:28 +0900

    day8

--- src/week4/day8/Day8DeepeningArray.java
+++ src/week4/day8/Day8DeepeningArray.java
@@ -20,12 +20,34 @@ public class Day8DeepeningArray {
         Arrays.sort(scores);
         System.out.println("정렬된 점수: " + Arrays.toString(scores));
 
+        // 배열 내림차순 정렬
+        for (int i = 0; i < scores.length / 2; i++) {
+            int temp = scores[i];
+            scores[i] = scores[scores.length - i - 1];
+            scores[scores.length - i - 1] = temp;
+        }
+        System.out.println("내림차순 정렬 = " + Arrays.toString(scores));
+
         // 다차원 배열
         int[][] studentScores = {
                 {85, 90, 92}, // 학생 1
                 {78, 88, 80}, // 학생 2
                 {90, 94, 99}  // 학생 3
         };
+
+        int[][] ss = new int[3][4];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                ss[i][j] = i + j;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(ss[i][j] + " ");
+            }
+            System.out.println();
+        }
 
         // 학생 점수 출력
         for (int i = 0; i < studentScores.length; i++) {
