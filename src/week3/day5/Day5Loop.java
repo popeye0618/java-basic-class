8009f548728b0abaaba80ea8c69c9bcc588c84da
popeye0618 <popeye0618@gmail.com>
2024-12-15 12:11:17 +0900

    day5

--- src/week3/day5/Day5Loop.java
+++ src/week3/day5/Day5Loop.java
@@ -5,6 +5,7 @@ public class Day5Loop {
 
         // for 반복문
         System.out.println("for 반복문 예제:");
+
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
@@ -15,14 +16,22 @@ public class Day5Loop {
 
         // while 반복문
         System.out.println("\nwhile 반복문 예제:");
+
         int count = 1;
         while (count <= 5) {
             System.out.println("count = " + count);
             count++;
         }
 
+        int countt = 5;
+        while (countt >= 1) {
+            System.out.println("countt = " + countt);
+            countt--;
+        }
+
         // do-while 반복문
         System.out.println("\ndo-while 반복문 예제:");
+
         int number = 1;
         do {
             System.out.println("number = " + number);
--- src/week3/day5/Day5Practice.java
+++ src/week3/day5/Day5Practice.java
@@ -3,40 +3,44 @@ package week3.day5;
 import java.util.Scanner;
 
 public class Day5Practice {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        // 1부터 10까지 for문, while문을 사용해서 출력
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("i = " + i);
-        }
+		// 1부터 10까지 for문, while문을 사용해서 출력
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("i = " + i);
+		}
 
-        int n = 1;
-        while (n <= 10) {
-            System.out.println("n = " + n);
-            n++; // n += 1
-        }
+		int n = 1;
+		while (n <= 10) {
+			System.out.println("n = " + n);
+			n++; // n += 1
+		}
 
+		// 조건문 사용하지 않고 1부터 10까지 홀수 출력하기
+		for (int i = 1; i <= 10; i += 2) {
+			System.out.println("i = " + i);
+		}
 
-        // 구구단 출력
-        System.out.print("출력할 단을 입력하세요 (2~9): ");
-        int num = sc.nextInt();
+		// 구구단 출력
+		System.out.print("출력할 단을 입력하세요 (2~9): ");
+		int num = sc.nextInt();
 
-        if (num < 2 || num > 9) {
-            System.out.print("2에서 9 사이의 숫자를 입력하세요: ");
-        } else {
-            for (int i = 1; i <= 9; i++) {
-                System.out.println(num + " x " + i + " = " + (num * i));
-            }
-        }
+		if (num < 2 || num > 9) {
+			System.out.print("2에서 9 사이의 숫자를 입력하세요");
+		} else {
+			for (int i = 1; i <= 9; i++) {
+				System.out.println(num + " x " + i + " = " + (num * i));
+			}
+		}
 
-        // 구구단 거꾸로 출력
-        if (num < 2 || num > 9) {
-            System.out.print("2에서 9 사이의 숫자를 입력하세요: ");
-        } else {
-            for (int i = 9; i >= 1; i--) {
-                System.out.println(num + " x " + i + " = " + (num * i));
-            }
-        }
-    }
+		// 구구단 거꾸로 출력
+		if (num < 2 || num > 9) {
+			System.out.print("2에서 9 사이의 숫자를 입력하세요");
+		} else {
+			for (int i = 9; i >= 1; i--) {
+				System.out.println(num + " x " + i + " = " + (num * i));
+			}
+		}
+	}
 }
