1dba660ea4fad23e33bb229ff9ee038bbc28e0ed
popeye0618 <popeye0618@gmail.com>
2024-12-15 12:11:24 +0900

    day7

--- src/week4/day7/Day7Array.java
+++ src/week4/day7/Day7Array.java
@@ -13,13 +13,21 @@ public class Day7Array {
         System.out.println("ints[4] : " + ints[4]);
         System.out.println("배열의 크기는 : " + ints.length);
 
+        ints[3] = 1;
+
         // 배열 값 출력
         for (int i = 0; i < ints.length; i++) {
             System.out.println("Student " + (i + 1) + ": " + ints[i]);
         }
 
         // 문자 배열
-        char[] chars = {'c', 'h', 'a', 'r'};
+        char[] chars = new char[4];
+
+        chars[0] = 'c';
+        chars[1] = 'h';
+        chars[2] = 'a';
+        chars[3] = 'r';
+
         for (int i = 0; i < chars.length; i++) {
             System.out.println("Student " + (i + 1) + ": " + chars[i]);
         }
