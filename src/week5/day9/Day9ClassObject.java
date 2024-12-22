d354ef75c906526d8b9d894692ece95a9be757d4
popeye0618 <popeye0618@gmail.com>
2024-12-23 00:38:59 +0900

    day9

--- src/week5/day9/Day9ClassObject.java
+++ src/week5/day9/Day9ClassObject.java
@@ -8,11 +8,18 @@ public class Day9ClassObject {
     public static void main(String[] args) {
 
         // Dog 클래스를 이용해 객체 (인스턴스) 만들기
-        Dog dog = new Dog();
-        dog.name = "poppy";
-        dog.age = 4;
+        Dog dog1 = new Dog();
+        dog1.name = "김율";
+        dog1.age = 2;
 
-        System.out.println("강아지 이름 = " + dog.name);
-        System.out.println("강아지 나이 = " + dog.age);
+        System.out.println("강아지 이름 = " + dog1.name);
+        System.out.println("강아지 나이 = " + dog1.age);
+
+        Dog dog2 = new Dog();
+        dog2.name = "뽀삐";
+        dog2.age = 4;
+
+        System.out.println("강아지 이름 = " + dog2.name);
+        System.out.println("강아지 나이 = " + dog2.age);
     }
 }
