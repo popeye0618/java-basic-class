3ff79faa321e22a3d6c37c3f6320272b3f4752a1
popeye0618 <popeye0618@gmail.com>
2025-02-19 11:03:12 +0900

    day4

--- src/week2/day4/Day4Practice.java
+++ src/week2/day4/Day4Practice.java
@@ -41,17 +41,18 @@ public class Day4Practice {
 
         String season;
 
-        switch (month) {
-            case 12: case 1: case 2:
+        int weather = (month % 12) / 3;
+        switch (weather) {
+            case 0:
                 season = "겨울";
                 break;
-            case 3: case 4: case 5:
+            case 1:
                 season = "봄";
                 break;
-            case 6: case 7: case 8:
+            case 2:
                 season = "여름";
                 break;
-            case 9: case 10: case 11:
+            case 3:
                 season = "가을";
                 break;
             default:
