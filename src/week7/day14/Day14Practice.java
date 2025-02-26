b19e187b2dcc617bc054ed6725020ecff4494591
popeye0618 <popeye0618@gmail.com>
2025-02-26 11:55:00 +0900

    day14

--- src/week7/day14/Day14Practice.java
+++ src/week7/day14/Day14Practice.java
@@ -38,26 +38,21 @@ public class Day14Practice {
 		System.out.print("높이를 입력하세요: ");
 		int n = sc.nextInt();
 
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n-i-1; j++) {
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
 				System.out.print(" ");
 			}
-			for(int j=0; j<i+1; j++) {
-				System.out.print("*");
-			}
-			for(int j=0; j<i; j++) {
+			for (int j = 1; j <= (2 * i - 1); j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-		for(int i=0; i<n-1; i++) {
-			for(int j=0; j<i+1; j++) {
+		for (int i = n - 1; i > 0; i--) {
+			for (int j = 1; j <= n - i; j++) {
 				System.out.print(" ");
 			}
-			for(int j=0; j<n-1-i; j++) {
-				System.out.print("*");
-			}
-			for(int j=0; j<n-2-i; j++) {
+			for (int j = (2 * i - 1); j > 0; j--) {
 				System.out.print("*");
 			}
 			System.out.println();
